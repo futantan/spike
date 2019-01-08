4c2aea3bef111cf4124d6ea53d1df197e055c824
Tantan Fu <luckytantanfu@gmail.com>
2019-01-08 18:09:08 +0800

    update

---
+++ books/Test-Driving-JavaScript-Applications/tdjsa/tdd/client/palindrome/test/palindrome-test.js
@@ -0,0 +1,38 @@
+describe("palindrome-test", function () {
+  it("should pass this canary test", function () {
+    expect(true).to.be.true;
+  });
+
+  it("should return true for argument mom", function () {
+    expect(isPalindrome("mom")).to.be.true;
+  })
+
+  it("should return true for argument dad", function () {
+    expect(isPalindrome("dad")).to.be.true;
+  })
+
+  it("should return false for argument dude", function () {
+    expect(isPalindrome("dude")).to.be.false;
+  })
+
+  it("should return true for argument mom mom", function () {
+    expect(isPalindrome("mom mom")).to.be.true;
+  })
+
+  it("should return false for argument mom dad", function () {
+    expect(isPalindrome("mom dad")).to.be.false;
+  })
+
+  it("should return false when argument is an empty string", function () {
+    expect(isPalindrome("")).to.be.false;
+  })
+
+  it("should return false for argument with only two spaces", function () {
+    expect(isPalindrome("  ")).to.be.false;
+  })
+
+  it("should throw an exception if argument is missing", function () {
+    var call = function () { isPalindrome(); }
+    expect(call).to.throw(Error, 'Invalid argument');
+  });
+});
