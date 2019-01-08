5d4c2ab5aa7a109a918e1ba0dea86e93b68afdee
Tantan Fu <luckytantanfu@gmail.com>
2019-01-08 18:16:10 +0800

    springboot in action

---
+++ books/sprintboot-in-action/readinglist/src/main/java/com/example/readinglist/ReadingListApplication.java
@@ -0,0 +1,14 @@
+package com.example.readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingListApplication.class, args);
+	}
+
+}
+
---
+++ books/sprintboot-in-action/readinglist/src/main/java/com/example/readinglist/ReadingListRepository.java
@@ -0,0 +1,8 @@
+package com.example.readinglist;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReadingListRepository extends JpaRepository<Book, Long> {
+  List<Book> findByReader(String reader);
+}
