44294d24345a5c47023b8054adc7ddf63f3d6f87
futantan <luckytantanfu@gmail.com>
2019-01-24 21:20:40 +0800

    Degenerate Objects

--- books/tdd/src/main/java/money/Dollar.java
+++ books/tdd/src/main/java/money/Dollar.java
@@ -8,7 +8,7 @@ class Dollar {
     this.amount = amount;
   }
 
-  void times(int multiplier) {
-    amount = amount * multiplier;
+  Dollar times(int multiplier) {
+    return new Dollar(amount * multiplier);
   }
 }
--- books/tdd/src/test/java/money/DollarTest.java
+++ books/tdd/src/test/java/money/DollarTest.java
@@ -8,7 +8,9 @@ public class DollarTest {
   @Test
   public void testMultiplication() {
     Dollar five = new Dollar(5);
-    five.times(2);
-    assertEquals(10, five.amount);
+    Dollar product = five.times(2);
+    assertEquals(10, product.amount);
+    product = five.times(3);
+    assertEquals(15, product.amount);
   }
 }
