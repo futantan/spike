9878aef6226f3431825ffe0bbe1abee4aaef29d6
Tantan Fu <luckytantanfu@gmail.com>
2019-01-28 11:17:23 +0800

    TDD: chap 4 - Privacy

--- books/tdd/src/test/java/money/DollarTest.java
+++ books/tdd/src/test/java/money/DollarTest.java
@@ -7,13 +7,12 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 public class DollarTest {
+
   @Test
   public void testMultiplication() {
     Dollar five = new Dollar(5);
-    Dollar product = five.times(2);
-    assertEquals(10, product.amount);
-    product = five.times(3);
-    assertEquals(15, product.amount);
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(15), five.times(3));
   }
 
   @Test
