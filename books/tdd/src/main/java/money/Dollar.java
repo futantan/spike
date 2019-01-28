ff4f2aa150e6e33c032301f24595da6adc66cdde
Tantan Fu <luckytantanfu@gmail.com>
2019-01-28 15:17:24 +0800

    tdd - Chapter 9. Times We're Livin' In

--- books/tdd/src/main/java/money/Dollar.java
+++ books/tdd/src/main/java/money/Dollar.java
@@ -2,11 +2,11 @@ package money;
 
 class Dollar extends Money {
 
-  Dollar(int amount) {
-    this.amount = amount;
+  Dollar(int amount, String currency) {
+    super(amount, currency);
   }
 
   Money times(int multiplier) {
-    return new Dollar(amount * multiplier);
+    return Money.dollar(amount * multiplier);
   }
 }
--- books/tdd/src/main/java/money/Money.java
+++ books/tdd/src/main/java/money/Money.java
@@ -3,16 +3,26 @@ package money;
 abstract class Money {
 
   int amount;
+  private String currency;
+
+  Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
 
   static Money dollar(int amount) {
-    return new Dollar(amount);
+    return new Dollar(amount, "USD");
   }
 
   static Money franc(int amount) {
-    return new Franc(amount);
+    return new Franc(amount, "CHF");
   }
 
   abstract Money times(int multiplier);
+
+  String currency() {
+    return currency;
+  }
 
   public boolean equals(Object obj) {
     Money money = (Money) obj;
