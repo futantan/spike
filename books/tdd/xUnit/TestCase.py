88d2ed7c3491553d26bffa59107b56001ffbb38c
futantan <luckytantanfu@gmail.com>
2019-02-11 20:51:22 +0800

    tdd - Chapter 18. First Steps to xUnit

---
+++ books/tdd/xUnit/TestCase.py
@@ -0,0 +1,24 @@
+class TestCase:
+  def __init__(self, name):
+    self.name = name
+
+  def run(self):
+    method = getattr(self, self.name)
+    method()
+
+class WasRun(TestCase):
+  def __init__(self, name):
+    self.wasRun = None
+    TestCase.__init__(self, name)
+
+  def testMethod(self):
+    self.wasRun = 1
+
+class TestCaseTest(TestCase):
+  def testRunning(self):
+    test = WasRun("testMethod")
+    assert(not test.wasRun)
+    test.run()
+    assert(test.wasRun)
+
+TestCaseTest("testRunning").run()
