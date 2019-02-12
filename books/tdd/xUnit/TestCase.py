e63ca7429c1756c3e3d84a419824be78f34d57dd
futantan <luckytantanfu@gmail.com>
2019-02-12 21:20:10 +0800

    tdd - Chapter 19. Set the Table

--- books/tdd/xUnit/TestCase.py
+++ books/tdd/xUnit/TestCase.py
@@ -2,23 +2,36 @@ class TestCase:
   def __init__(self, name):
     self.name = name
 
+  def setUp(self):
+    pass
+  
   def run(self):
+    self.setUp()
     method = getattr(self, self.name)
     method()
 
 class WasRun(TestCase):
   def __init__(self, name):
-    self.wasRun = None
     TestCase.__init__(self, name)
+
+  def setUp(self):
+    self.wasRun = None
+    self.wasSetup = 1
 
   def testMethod(self):
     self.wasRun = 1
 
 class TestCaseTest(TestCase):
+  def run(self):
+    self.test = WasRun("testMethod")
+
+  def testSetup(self):
+    test.run()
+    assert(test.wasSetup)
+
   def testRunning(self):
-    test = WasRun("testMethod")
-    assert(not test.wasRun)
     test.run()
     assert(test.wasRun)
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetup").run()
