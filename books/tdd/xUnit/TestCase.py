d97d4282dfe59d4783195606aa49374d1f94c1ec
futantan <luckytantanfu@gmail.com>
2019-02-13 22:07:29 +0800

    tdd - Chapter 21. Counting

--- books/tdd/xUnit/TestCase.py
+++ books/tdd/xUnit/TestCase.py
@@ -1,3 +1,13 @@
+class TestResult:
+  def __init__(self):
+    self.runCount = 0
+
+  def testStarted(self):
+    self.runCount = self.runCount + 1
+
+  def summary(self):
+    return "%d run, 0 failed" % self.runCount
+
 class TestCase:
   def __init__(self, name):
     self.name = name
@@ -6,10 +16,13 @@ class TestCase:
     pass
   
   def run(self):
+    result = TestResult()
+    result.testStarted()
     self.setUp()
     method = getattr(self, self.name)
     method()
     self.tearDown()
+    return result
 
   def tearDown(self):
     pass
@@ -25,16 +38,30 @@ class WasRun(TestCase):
   def testMethod(self):
     self.wasRun = 1
     self.log = self.log + "testMethod "
+  
+  def testBrokenMethod(self):
+    raise Exception
 
   def tearDown(self):
     self.log = self.log + "tearDown "
 
 class TestCaseTest(TestCase):
-  def run(self):
+  def setUp(self):
     self.test = WasRun("testMethod")
-
+  
+  def testResult(self):
+    test = WasRun("testMethod")
+    result = self.test.run()
+    assert("1 run, 0 failed" == result.summary())
+  
+  def testFailedResult(self):
+    test = WasRun("testBrokenMethod")
+    result = test.run()
+    assert("1 run, 1 failed", result.summary())
+    
   def testTemplateMethod(self):
     self.test.run()
     assert("setUp testMethod tearDown " == self.test.log)
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
