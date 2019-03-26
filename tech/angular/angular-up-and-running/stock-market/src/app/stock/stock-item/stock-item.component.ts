78c4fc54041f9ee46837028961cde5f6dff356d9
Tantan Fu <luckytantanfu@gmail.com>
2019-03-26 15:15:14 +0800

    using ngClass

--- tech/angular/angular-up-and-running/stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ tech/angular/angular-up-and-running/stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -9,11 +9,20 @@ import { Stock } from 'src/app/model/stock';
 export class StockItemComponent implements OnInit {
 
   public stock: Stock;
+  public stockClasses;
 
   constructor() { }
 
   ngOnInit() {
     this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+    const diff = (this.stock.price / this.stock.previousPrice) - 1;
+    const largeChange = Math.abs(diff) > 0.01;
+    this.stockClasses = {
+      positive: this.stock.isPositiveChange(),
+      negative: !this.stock.isPositiveChange(),
+      'large-change': largeChange,
+      'small-change': !largeChange,
+    };
   }
 
   toggleFavorite() {
