6471c1168bbef31be09bda0193dc5e0dec1b6b87
Tantan Fu <luckytantanfu@gmail.com>
2019-03-26 15:20:08 +0800

    use ngStyle

--- tech/angular/angular-up-and-running/stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ tech/angular/angular-up-and-running/stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -9,7 +9,7 @@ import { Stock } from 'src/app/model/stock';
 export class StockItemComponent implements OnInit {
 
   public stock: Stock;
-  public stockClasses;
+  public stockStyles;
 
   constructor() { }
 
@@ -17,11 +17,9 @@ export class StockItemComponent implements OnInit {
     this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
     const diff = (this.stock.price / this.stock.previousPrice) - 1;
     const largeChange = Math.abs(diff) > 0.01;
-    this.stockClasses = {
-      positive: this.stock.isPositiveChange(),
-      negative: !this.stock.isPositiveChange(),
-      'large-change': largeChange,
-      'small-change': !largeChange,
+    this.stockStyles = {
+      color: this.stock.isPositiveChange() ? 'green' : 'red',
+      'font-size': largeChange ? '1.2em' : '0.8em',
     };
   }
 
