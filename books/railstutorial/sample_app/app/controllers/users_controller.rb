6b9bb88023ecb6857fadeedf4c436f5b042193b9
Tantan Fu <luckytantanfu@gmail.com>
2019-04-29 21:06:11 +0800

    rails-sampleapp Add account activation

--- books/railstutorial/sample_app/app/controllers/users_controller.rb
+++ books/railstutorial/sample_app/app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = 'Welcome to the Sample App!'
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account"
+      redirect_to root_url
     else
       render 'new'
     end
