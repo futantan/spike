57e7b93a12f1f5a3d6430b602a1ddce45f310946
Tantan Fu <luckytantanfu@gmail.com>
2019-05-01 21:16:01 +0800

    rails-sampleapp Add password reset

--- books/railstutorial/sample_app/config/routes.rb
+++ books/railstutorial/sample_app/config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
