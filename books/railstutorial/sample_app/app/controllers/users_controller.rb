432fc21480ab7d0c4a373de5a47d7bda060798b8
Tantan Fu <luckytantanfu@gmail.com>
2019-05-05 21:26:14 +0800

    rails-sampleapp Add user microposts

--- books/railstutorial/sample_app/app/controllers/users_controller.rb
+++ books/railstutorial/sample_app/app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -50,18 +51,8 @@ class UsersController < ApplicationController
     redirect_to(root_url) unless current_user.admin?
   end
 
-  private
-
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
   end
 
   def correct_user
---
+++ books/railstutorial/sample_app/test/integration/microposts_interface_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "micropost interface" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+    assert_select 'input[type=file]'
+    # 无效提交
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "" } }
+    end
+    assert_select 'div#error_explanation'
+    # 有效提交
+    content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, params: { micropost: { content: content, picture: picture } }
+    end
+    assert assigns(:micropost).picture?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match content, response.body # 删除一篇微博
+    assert_select 'a', text: 'delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+    # 访问另一个用户的资料页面(没有删除链接)
+    get user_path(users(:archer))
+    assert_select 'a', text: 'delete', count: 0
+  end
+
+end
