ae5cad0e15d43f3ff6dd34e9218af42f4ab8b8ff
Tantan Fu <luckytantanfu@gmail.com>
2019-05-06 21:45:55 +0800

    rails-sampleapp Add user following

--- books/railstutorial/sample_app/test/models/user_test.rb
+++ books/railstutorial/sample_app/test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com",
+    @user = User.new(name:     "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: 'foobar')
   end
 
@@ -47,7 +47,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'email address should be unique' do
-    duplicate_user = @user.dup
+    duplicate_user       = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
@@ -55,7 +55,7 @@ class UserTest < ActiveSupport::TestCase
 
   test 'email address should be saved as lower-case' do
     mixed_case_email = 'Foo@ExAMPle.CoM'
-    @user.email = mixed_case_email
+    @user.email      = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
@@ -79,6 +79,35 @@ class UserTest < ActiveSupport::TestCase
     @user.microposts.create!(content: 'Lorem ipsum')
     assert_difference 'Micropost.count', -1 do
       @user.destroy
+    end
+  end
+
+  test 'should follow and unfollow a user' do
+    michael = users(:michael)
+    archer  = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # 关注的用户发布的微博
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # 自己的微博
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # 未关注用户的微博
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
     end
   end
 end
