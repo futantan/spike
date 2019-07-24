9e29525ed99cbe86bda8ad49adfe0cf65b4f2ad4
futantan <luckytantanfu@gmail.com>
2019-07-24 16:18:41 +0800

    add category widget

---
+++ tech/flutter/Build-Native-Mobile-Apps-with-Flutter/hello_rectangle/lib/category.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+final _rowHeight = 100.0;
+
+/// A custom [Category] widget.
+///
+/// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
+/// a colored [InkWell] animation.
+class Category extends StatelessWidget {
+  final ColorSwatch color;
+  final String name;
+  final IconData iconLocation;
+
+  /// Creates a [Category].
+  ///
+  /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
+  /// the UI, and the icon that represents it (e.g. a ruler).
+  const Category({this.name, this.color, this.iconLocation});
+
+  /// Builds a custom widget that shows [Category] information.
+  ///
+  /// This information includes the icon, name, and color for the [Category].
+  @override
+  // This `context` parameter describes the location of this widget in the
+  // widget tree. It can be used for obtaining Theme data from the nearest
+  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
+  // See https://docs.flutter.io/flutter/material/Theme-class.html
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _rowHeight,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(50),
+          highlightColor: color,
+          splashColor: color,
+          onTap: () {
+            print('I was tapped!');
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Icon(
+                    iconLocation,
+                    size: 60,
+                  ),
+                ),
+                Center(
+                    child: Text(
+                  name,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline,
+                ))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
