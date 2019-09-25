dc25aabdbc7dfde799117afc71d3de7d60dd69d9
Tantan Fu <luckytantanfu@gmail.com>
2019-09-25 18:07:17 +0800

    pragstudio elixir chap16

--- tech/pragstudio-elixir/servy/lib/servy/bear_controller.ex
+++ tech/pragstudio-elixir/servy/lib/servy/bear_controller.ex
@@ -3,20 +3,20 @@ defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
 
-  def index(conv) do
-    items =
-      Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
-      |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join()
+  @templates_path Path.expand("../../templates", __DIR__)
 
-    %Conv{conv | status: 200, resp_body: "<ul>#{items}</ul>"}
+  def index(conv) do
+    bears =
+      Wildthings.list_bears()
+      |> Enum.sort(&Bear.order_asc_by_name/2)
+
+    render(conv, "index.html.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    %Conv{conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name} </h1>"}
+
+    render(conv, "show.html.eex", bear: bear)
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
@@ -31,7 +31,12 @@ defmodule Servy.BearController do
     %Conv{conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
   end
 
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
+  defp render(conv, template, bindings) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %Conv{conv | status: 200, resp_body: content}
   end
 end
