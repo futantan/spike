6da4954e75bc585ad930ce4153df930270916aac
futantan <luckytantanfu@gmail.com>
2022-07-10 21:18:58 +0800

    finish

--- tech/web3/Build-a-Web3-app-with-Solidity/my-wave-portal/hardhat.config.js
+++ tech/web3/Build-a-Web3-app-with-Solidity/my-wave-portal/hardhat.config.js
@@ -1,6 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
+  networks: {
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
---
+++ tech/web3/Build-a-Web3-app-with-Solidity/my-wave-portal/scripts/deploy.js
@@ -0,0 +1,21 @@
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.001"),
+  });
+  await waveContract.deployed();
+
+  console.log("WavePortal address: ", waveContract.address);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+runMain();
