9ebe7ba68d26f46ba7939cedcb9bd0e4cc0d8de8
futantan <luckytantanfu@gmail.com>
2022-12-18 22:08:24 +0800

    Vue.js设计与实现 - renderer

--- books/Vue.js设计与实现/chap-03/3.2.js
+++ books/Vue.js设计与实现/chap-03/3.2.js
@@ -1,5 +1,15 @@
+const MyComponent = function () {
+  return {
+    tag: 'div',
+    props: {
+      onClick: () => alert('hello'),
+    },
+    children: 'click me'
+  }
+}
+
 const vnode = {
-  tag: 'div',
+  tag: MyComponent,
   props: {
     onClick: () => alert('hello'),
   },
@@ -7,6 +17,16 @@ const vnode = {
 }
 
 function renderer(vnode ,container) {
+  if (typeof vnode.tag === 'string') {
+    // 说明 vnode 描述的是标签元素
+    mountElement(vnode, container)
+  } else if (typeof vnode.tag === 'function') {
+    // 说明 vnode 描述的是组件
+    mountComponent(vnode, container)
+  }
+}
+
+function mountElement(vnode, container) {
   // 使用 vnode.tag 作为标签名创建 DOM 元素
   const el = document.createElement(vnode.tag)
   // 遍历 vnode.props 对象，将属性、事件添加到 DOM 元素上
@@ -29,6 +49,13 @@ function renderer(vnode ,container) {
 
   // 将元素添加到挂载点下
   container.appendChild(el)
+}
+
+function mountComponent(vnode, container) {
+  // 调用组件函数，获取组件要渲染的内容（虚拟 DOM）
+  const subtree = vnode.tag()
+  // 递归地调用 renderer 渲染 subtree
+  renderer(subtree, container)
 }
 
 renderer(vnode, document.body) // body 作为挂载点
