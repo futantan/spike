3fd8d5dcb30a5c804055d4b4639e78cfc17b74e0
futantan <luckytantanfu@gmail.com>
2024-03-03 22:59:26 +0800

    eth 04

--- tech/web3/wtf-ethersjs/src/04.js
+++ tech/web3/wtf-ethersjs/src/04.js
@@ -4,22 +4,31 @@ import { ethers } from 'ethers';
 dotenv.config();
 
 const mainnetKey = process.env.mainnetKey;
-const provider = new ethers.providers.JsonRpcProvider(mainnetKey);
+const provider = new ethers.JsonRpcProvider(mainnetKey);
 
-const wallet = ethers.Wallet.createRandom().connect(provider);
+// 1. 创建随机的 wallet 对象
+const wallet1 = ethers.Wallet.createRandom();
 
-console.log(`钱包助记词: ${wallet.mnemonic.phrase}`);
-console.log(`钱包私钥: ${wallet.privateKey}`);
-const txCount = await wallet.getTransactionCount();
+// 2. 利用私钥和provider创建wallet对象
+const privateKey =
+  '0x227dbb8586117d55284e26620bc76534dfbd2394be34cf4a09cb775d593b6f2b';
+const wallet2 = new ethers.Wallet(privateKey, provider);
+
+// 3. 利用助记词创建wallet对象
+const wallet3 = ethers.Wallet.fromPhrase(wallet1.mnemonic.phrase);
+
+console.log(`钱包助记词: ${wallet1.mnemonic.phrase}`);
+console.log(`钱包私钥: ${wallet1.privateKey}`);
+const txCount = await wallet1.getTransactionCount();
 console.log(`钱包发送交易次数: ${txCount}`);
 
 // 创建交易请求，参数：to为接收地址，value为ETH数额
 const tx = {
   to: address1,
-  value: ethers.utils.parseEther('0.001'),
+  value: ethers.parseEther('0.001'),
 };
 
 //发送交易，获得收据
-const receipt = await wallet.sendTransaction(tx);
+const receipt = await wallet1.sendTransaction(tx);
 await receipt.wait(); // 等待链上确认交易
 console.log(receipt); // 打印交易详情
