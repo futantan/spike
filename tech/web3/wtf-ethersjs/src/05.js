1ef8262b52dfce9418d8feda54204b36d6eb36e7
futantan <luckytantanfu@gmail.com>
2024-03-03 23:09:48 +0800

    feat: 🎸 05js

--- tech/web3/wtf-ethersjs/src/05.js
+++ tech/web3/wtf-ethersjs/src/05.js
@@ -2,15 +2,15 @@
 
 import { ethers } from 'ethers';
 
-// 利用Infura的rpc节点连接以太坊网络
-const INFURA_ID = '';
-// 连接Rinkeby测试网
-const provider = new ethers.providers.JsonRpcProvider(
-  `https://rinkeby.infura.io/v3/${INFURA_ID}`
-);
+// 利用Alchemy的rpc节点连接以太坊网络
+const ALCHEMY_GOERLI_URL =
+  'https://eth-goerli.alchemyapi.io/v2/GlaeWuylnNM3uuOo-SAwJxuwTdqHaY5l';
+
+const provider = new ethers.JsonRpcProvider(ALCHEMY_GOERLI_URL);
 
 // 利用私钥和provider创建wallet对象
-const privateKey = 'xxxxxxxxx';
+const privateKey =
+  '0x227dbb8586117d55284e26620bc76534dfbd2394be34cf4a09cb775d593b6f2b';
 const wallet = new ethers.Wallet(privateKey, provider);
 
 // WETH的ABI
@@ -20,31 +20,32 @@ const abiWETH = [
   'function transfer(address, uint) public returns (bool)',
   'function withdraw(uint) public',
 ];
-// WETH合约地址（Rinkeby测试网）
-const addressWETH = '0xc778417e063141139fce010982780140aa0cd5ab'; // WETH Contract
+// WETH合约地址（Goerli测试网）
+const addressWETH = '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6'; // WETH Contract
 
 // 声明可写合约
 const contractWETH = new ethers.Contract(addressWETH, abiWETH, wallet);
+// 也可以声明一个只读合约，再用connect(wallet)函数转换成可写合约。
+// const contractWETH = new ethers.Contract(addressWETH, abiWETH, provider)
+// contractWETH.connect(wallet)
 
 const address = await wallet.getAddress();
 // 读取WETH合约的链上信息（WETH abi）
 console.log('\n1. 读取WETH余额');
 const balanceWETH = await contractWETH.balanceOf(address);
-console.log(`存款前WETH持仓: ${ethers.utils.formatEther(balanceWETH)}\n`);
+console.log(`存款前WETH持仓: ${ethers.formatEther(balanceWETH)}\n`);
 
 console.log('\n2. 调用desposit()函数，存入0.001 ETH');
 // 发起交易
 const tx = await contractWETH.deposit({
-  value: ethers.utils.parseEther('0.001'),
+  value: ethers.parseEther('0.001'),
 });
 // 等待交易上链
 await tx.wait();
 console.log(`交易详情：`);
 console.log(tx);
 const balanceWETH_deposit = await contractWETH.balanceOf(address);
-console.log(
-  `存款后WETH持仓: ${ethers.utils.formatEther(balanceWETH_deposit)}\n`
-);
+console.log(`存款后WETH持仓: ${ethers.formatEther(balanceWETH_deposit)}\n`);
 
 console.log('\n3. 调用transfer()函数，给vitalik转账0.001 WETH');
 // 发起交易
